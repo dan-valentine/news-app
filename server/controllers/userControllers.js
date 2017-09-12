467cc69007270f63507cd2c5486ab042570178d1
Daniel Valentine <mrdanvalentine@gmail.com>
2017-09-12 16:30:45 -0600

    added more to user controller

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -5,8 +5,17 @@ module.exports = {
         const {username, email, name, img} = req.body;
 
         dbInstance.editUser([userID, username, email, name, img]).then(user =>{
+            req.session.user = {
+                userID: userID,
+                username: username, 
+                email: email, 
+                name: name, 
+                img : img
+            }
             res.status(200).send(user);
         })
+    }, getUSer: (req, res) => {
+        res.status(200).send(req.session.user);
     }
     
 }
